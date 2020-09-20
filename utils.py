75737df4d1c1dea74bbaef95af937928c61e51e9
Aakanksha Dubey <dubey1aakanksha@gmail.com>
2020-09-20 18:34:07 +0530

    Rouge Function

--- utils.py
+++ utils.py
@@ -1,1 +1,15 @@
 # define utility tools (functions here) like rogue (metrics) , loss functions etc.
+
+
+# Rouge 
+from rouge import Rouge
+
+rouge = Rouge(metrics = ['rouge-1','rouge-2', 'rouge-l'])
+
+
+# Function to compare created summary with reference summary
+def evaluateSummary(hypothesis, reference) :
+    scores = rouge.get_scores(hypothesis, reference)
+    return scores
+
+# scores =  evaluateSummary(hypothesis, reference)
