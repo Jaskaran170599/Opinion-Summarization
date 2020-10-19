57c430d2d1a2a98d6e3fae8467983d0fb89435a9
Jaskaran Singh <jaskaransingh101010@gmail.com>
2020-10-19 22:17:04 +0530

    model.py

--- model.py
+++ model.py
@@ -1,1 +1,7 @@
 # define the model structure here , use TF. 
+import torch
+from transformers import EncoderDecoderModel
+from configs import config
+
+def get_model(model=0):
+    return EncoderDecoderModel.from_encoder_decoder_pretrained(config.MODEL_LIST[model],config.MODEL_LIST[model])
