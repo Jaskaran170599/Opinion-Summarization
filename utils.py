f2c04f8c29f3e14f9fc326479a39b2a1052e1cc3
Aakanksha Dubey <dubey1aakanksha@gmail.com>
2020-11-08 00:45:01 +0530

    Rouge score changes

--- utils.py
+++ utils.py
@@ -10,6 +10,11 @@ rouge = Rouge(metrics = ['rouge-1','rouge-2', 'rouge-l'])
 # Function to compare created summary with reference summary
 def evaluateSummary(hypothesis, reference) :
     scores = rouge.get_scores(hypothesis, reference)
-    return scores
+    new_score = {
+        'Rouge-1' : scores[0]['rouge-1']['f'],
+        'Rouge-2' : scores[0]['rouge-2']['f'],
+        'Rouge-L' : scores[0]['rouge-l']['f'],
+    }
+    return new_score
 
 # scores =  evaluateSummary(hypothesis, reference)
